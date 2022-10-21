973e44b60d73775839c7753f48f04e382e451fe3
Sofia Dersén <Sofia_dersen@hotmail.com>
2022-10-21 13:27:10 +0200

    Change on getFileFromStorage
    and playback from all tracks at the same time. BUT no howl

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,22 +1,54 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Recorder from '../components/Recorder/Recorder';
 import { useAuth } from '../context/AuthContext';
 import { getFileFromStorage } from '../utils/getFileFromStorage';
+import ReactHowler from 'react-howler';
+import ReactAudioPlayer from 'react-audio-player';
 
 const Dashboard = () => {
   const { user } = useAuth();
-  const array = [];
+  const [testArray, setTestArray] = useState([]);
+  const [playing, setPlaying] = useState(false);
+  const [preload, setPreload] = useState(false);
+  const [audio, setAudio] = useState('example.mp3');
 
-  getFileFromStorage(user.uid, 1).then((res) => (array[0] = res));
-  getFileFromStorage(user.uid, 2).then((res) => (array[1] = res));
-  getFileFromStorage(user.uid, 3).then((res) => (array[2] = res));
-  getFileFromStorage(user.uid, 4).then((res) => (array[3] = res));
-  console.log(array);
+  useEffect(() => {
+    getFileFromStorage(user.uid).then((res) => setTestArray(res));
+  }, []);
+
+  const play = () => {
+    setPlaying(true);
+  };
+
+  const player1 = new Audio(testArray[0]);
+  const player2 = new Audio(testArray[1]);
+  const player3 = new Audio(testArray[2]);
+  const player4 = new Audio(testArray[3]);
+
+  const playAll = () => {
+    player1.play();
+    player2.play();
+    player3.play();
+    player4.play();
+  };
 
   return (
     <div>
       <p>This route is protected</p>
       <p> Well hello {user.email}!</p>
+      <audio src={testArray[0]}></audio>
+
+      <button onClick={() => player1.play()}>Play</button>
+      <button onClick={() => player1.pause()}>Stop</button>
+      <button onClick={() => player2.play()}>Play2</button>
+      <button onClick={() => player2.pause()}>Stop2</button>
+      <button onClick={() => player3.play()}>Play3</button>
+      <button onClick={() => player3.pause()}>Stop3</button>
+      <button onClick={() => player4.play()}>Play4</button>
+      <button onClick={() => player4.pause()}>Stop4</button>
+      <button onClick={() => playAll()}>play all</button>
+
+      <h3>Recorders</h3>
       <Recorder id={1}></Recorder>
       <Recorder id={2}></Recorder>
       <Recorder id={3}></Recorder>
--- utils/getFileFromStorage.js
+++ utils/getFileFromStorage.js
@@ -1,23 +1,82 @@
 import { ref, getDownloadURL } from 'firebase/storage';
 import { storage } from './firebase';
 
-export const getFileFromStorage = async (userId, trackNo) => {
-  const filePath = `files/${userId}/${trackNo}/test`;
-  const storageRef = ref(storage, filePath);
+export const getFileFromStorage = async (userId) => {
+  // const filePath = `files/${userId}/${trackNo}/test`;
+  // const storageRef = ref(storage, filePath);
+  let trackArray = [];
 
-  const downloadUrl = await getDownloadURL(storageRef)
+  const downloadUrl = await getDownloadURL(
+    ref(storage, `files/${userId}/1/test`)
+  )
     .then((url) => {
-      const track = url;
-      return track;
+      // const track = url;
+      trackArray.push(url);
+      // return url;
     })
     .catch((error) => {
       if (error.code === 'storage/object-not-found') {
-        console.log(`tack Id ${trackNo}: nope`);
+        console.log(`tack Id 1: nope`);
+        trackArray.push('');
         return Promise.resolve('');
       } else {
         return Promise.reject(error);
       }
     });
 
-  return downloadUrl;
+  const downloadUrl2 = await getDownloadURL(
+    ref(storage, `files/${userId}/2/test`)
+  )
+    .then((url) => {
+      // const track = url;
+      trackArray.push(url);
+      // return url;
+    })
+    .catch((error) => {
+      if (error.code === 'storage/object-not-found') {
+        console.log(`tack Id 2: nope`);
+        trackArray.push('');
+        return Promise.resolve('');
+      } else {
+        return Promise.reject(error);
+      }
+    });
+
+  const downloadUrl3 = await getDownloadURL(
+    ref(storage, `files/${userId}/3/test`)
+  )
+    .then((url) => {
+      // const track = url;
+      trackArray.push(url);
+      // return url;
+    })
+    .catch((error) => {
+      if (error.code === 'storage/object-not-found') {
+        console.log(`tack Id 3: nope`);
+        trackArray.push('');
+        return Promise.resolve('');
+      } else {
+        return Promise.reject(error);
+      }
+    });
+
+  const downloadUrl4 = await getDownloadURL(
+    ref(storage, `files/${userId}/4/test`)
+  )
+    .then((url) => {
+      // const track = url;
+      trackArray.push(url);
+      // return url;
+    })
+    .catch((error) => {
+      if (error.code === 'storage/object-not-found') {
+        // console.log(`tack Id 4: nope`);
+        trackArray.push('');
+        return Promise.resolve('');
+      } else {
+        return Promise.reject(error);
+      }
+    });
+
+  return trackArray;
 };

3fc4778c4ed7ad481a464a7f5a97d31ae63c7527
Sofia Dersén <Sofia_dersen@hotmail.com>
2022-11-04 15:12:36 +0100

    Adjuster google btn and inputs

--- components/Navbar/navbar.jsx
+++ components/Navbar/navbar.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { useAuth } from '../../context/AuthContext';
-import { useRouter } from 'next/router';
 import Modal from '../Modal/Modal';
 import logo from '../../public/logo.svg';
 import styled from 'styled-components';
 
 export default function Navbar() {
-  const { user, logout } = useAuth();
-  const router = useRouter();
+  const { user } = useAuth();
 
   return (
     <NavWrapper>

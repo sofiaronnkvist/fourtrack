860530cfbc9d7668937296e5b3e95b5cc49f3e42
Sofia Dersén <Sofia_dersen@hotmail.com>
2022-11-07 20:29:53 +0100

    Get requested email from db

--- components/Shareproject/ShareProject.jsx
+++ components/Shareproject/ShareProject.jsx
@@ -1,77 +1,47 @@
 import { firestore } from '../../utils/firebase';
-import {
-  collection,
-  addDoc,
-  serverTimestamp,
-  getDocs,
-} from 'firebase/firestore';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 import { useAuth } from '../../context/AuthContext';
 import { useRouter } from 'next/router';
 
 export default function ShareProject() {
   const { user } = useAuth();
-  const [users, setUsers] = useState([]);
   const [searchText, setSearchText] = useState('');
   const [searchResult, setSearchResult] = useState([]);
 
-  useEffect(() => {
-    getUsers();
-  }, []);
-
   const getUsers = async () => {
     let array = [];
-    const querySnapshot = await getDocs(collection(firestore, 'users'));
-    console.log(`snapshot ${querySnapshot}`);
+    const q = query(
+      collection(firestore, 'users'),
+      where('email', '==', searchText)
+    );
+    const querySnapshot = await getDocs(q);
+
     querySnapshot.forEach((doc) => {
-      //   console.log(doc.id, ' => ', doc.data());
       array.push(doc.data());
     });
-    // console.log(array);
-    setUsers(array);
+    setSearchResult(array);
   };
-
-  console.log(`users outside fetch ${users}`);
-
-  useEffect(() => {
-    console.log(users);
-    const searchResult = users.find((element) => element == 'sofia');
-    console.log(`search result ${searchResult}`);
-  }, [searchText]);
+  console.log('users in outside UE', searchResult);
 
   return (
     <div>
-      <form>
-        <label>Project title</label>
-        <select>
-          {users &&
-            users.map((user, key) => {
-              return (
-                <option key={key} value='volvo'>
-                  {user.email}
-                </option>
-              );
-            })}
-          <option>hello</option>
-        </select>
+      <form onSubmit={getUsers}>
+        <label id='friend'>Find your friend</label>
         <input
           minLength='1'
           maxLength='50'
           onChange={(e) => setSearchText(e.target.value)}
-          type='text'
-          placeholder='Find your friend'
+          type='email'
+          placeholder='Enter email'
+          id='friend'
           required
         ></input>
-        <button type='submit'>Send invite</button>
       </form>
-      {/* <button onClick={getUsers}>get users</button> */}
+      <button onClick={getUsers}>Search</button>
       {searchResult &&
-        searchResult.map((user, key) => {
-          return (
-            <p key={key} value='volvo'>
-              {user.email}
-            </p>
-          );
+        searchResult.map((user) => {
+          return <p key={user.uid}>{user.email}</p>;
         })}
     </div>
   );

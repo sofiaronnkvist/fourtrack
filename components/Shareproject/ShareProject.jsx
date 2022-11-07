c026c6d9f5eb51601b97289b2a2463bf3a17c759
Sofia Dersén <Sofia_dersen@hotmail.com>
2022-11-07 14:00:54 +0100

    Start on search user

--- components/Shareproject/ShareProject.jsx
+++ components/Shareproject/ShareProject.jsx
@@ -5,48 +5,43 @@ import {
   serverTimestamp,
   getDocs,
 } from 'firebase/firestore';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useAuth } from '../../context/AuthContext';
 import { useRouter } from 'next/router';
 
 export default function ShareProject() {
   const { user } = useAuth();
   const [users, setUsers] = useState([]);
+  const [searchText, setSearchText] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   const getUsers = async () => {
-    console.log('klick!!!');
     let array = [];
     const querySnapshot = await getDocs(collection(firestore, 'users'));
     console.log(`snapshot ${querySnapshot}`);
     querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, ' => ', doc.data());
+      //   console.log(doc.id, ' => ', doc.data());
       array.push(doc.data());
     });
-    console.log(array);
+    // console.log(array);
     setUsers(array);
   };
-  console.log(users);
-  const createProject = async (e) => {
-    console.log('yup');
-    // e.preventDefault();
-    // try {
-    //   const projectsCollectionRef = collection(firestore, 'projects');
-    //   await addDoc(projectsCollectionRef, {
-    //     uid: user.uid,
-    //     title: project.title,
-    //     timestamp: serverTimestamp(),
-    //   });
-    //   setProject({ title: '' });
-    //   router.push('/projects');
-    // } catch (error) {
-    //   console.log(error);
-    //   alert(error);
-    // }
-  };
+
+  console.log(`users outside fetch ${users}`);
+
+  useEffect(() => {
+    console.log(users);
+    const searchResult = users.find((element) => element == 'sofia');
+    console.log(`search result ${searchResult}`);
+  }, [searchText]);
 
   return (
     <div>
-      <form onSubmit={createProject}>
+      <form>
         <label>Project title</label>
         <select>
           {users &&
@@ -59,23 +54,25 @@ export default function ShareProject() {
             })}
           <option>hello</option>
         </select>
-        {/* <input
-          value={project.title}
+        <input
           minLength='1'
-          maxLength='30'
-          onChange={(e) =>
-            setProject({
-              ...project,
-              title: e.target.value,
-            })
-          }
+          maxLength='50'
+          onChange={(e) => setSearchText(e.target.value)}
           type='text'
           placeholder='Find your friend'
           required
-        ></input> */}
+        ></input>
         <button type='submit'>Send invite</button>
       </form>
-      <button onClick={getUsers}>get users</button>
+      {/* <button onClick={getUsers}>get users</button> */}
+      {searchResult &&
+        searchResult.map((user, key) => {
+          return (
+            <p key={key} value='volvo'>
+              {user.email}
+            </p>
+          );
+        })}
     </div>
   );
 }

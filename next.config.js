72b24444967999101921a87391d5f8eaf4356f61
Sofia Dersén <Sofia_dersen@hotmail.com>
2022-11-11 15:18:52 +0100

    Purple icon on frontpage

--- next.config.js
+++ next.config.js
@@ -7,6 +7,8 @@ const nextConfig = {
   },
   images: {
     domains: ['lh3.googleusercontent.com'],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 };
 
